5c7b8d3492226e417e89d9e9edd7f0fca062645a
Jamie Fristrom <jafristr@microsoft.com>
2022-12-10 20:28:39 -0800

    Day 5 Part 2

--- aoc-5/src/main.rs
+++ aoc-5/src/main.rs
@@ -1,6 +1,18 @@
 use scanf::scanf;
 use std::io;
 
+fn get_blocks_to_grab( stack_from: &Vec<u8>, remaining: usize ) -> Vec<u8> {
+    let (_, blocks_to_grab) = stack_from.split_at(remaining);
+
+    blocks_to_grab.to_vec()
+}
+
+fn print_stacks(stacks: &Vec<Vec<u8>>) {
+    for stack in stacks {
+        println!("{:?}", stack);
+    }
+}
+
 fn main() {
     // input starting stacks
     let mut stacks: Vec<Vec<u8>> = vec![Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new()];
@@ -33,8 +45,8 @@ fn main() {
     }
     for stack in &mut stacks {
         stack.reverse();
-        println!("{:?}", stack);
     }
+    print_stacks(&stacks);
     // skip line
     let mut garbage_buffer = String::new();
     io::stdin().read_line(&mut garbage_buffer);
@@ -47,25 +59,20 @@ fn main() {
         if scanf!("move {} from {} to {}", number, column_from, column_to ).is_ok() {
             column_from -= 1;
             column_to -= 1;
-            for _ in 0..number {
-                println!("move from {column_from}");
-                match stacks[column_from].pop() {
-                    Some(block) => {
-                        println!("move to {column_to}");
-                        stacks[column_to].push(block);
-                    }
-                    None => {
-                        debug_assert!(false);
-                    }
-                }
-            }
+            let remaining = stacks[column_from].len()-number;
+            println!("number: {number} remaining: {remaining}");
+            let blocks_to_grab = get_blocks_to_grab(&stacks[column_from],remaining);
+            //let stack_from = &stacks[column_from];
+            //let blocks_to_grab_clone = blocks_to_grab.clone();
+            println!("Moving {:?} from {} to {}", blocks_to_grab, column_from, column_to);
+            let stack_to = &mut stacks[column_to];
+            stack_to.extend_from_slice(&blocks_to_grab);
+            stacks[column_from].truncate(remaining);
+            print_stacks(&stacks);
         }
         else {
             break;
         }
-    }
-    for stack in &stacks {
-        println!("{:?}", stack);
     }
     for stack in &mut stacks {
         match stack.pop() {
