432a09c4754526e3a0ae24ed07206ed97d62eedc
Jamie Fristrom <jafristr@microsoft.com>
2022-12-19 08:47:07 -0800

    Aoc 19 Partway through part 1, about to try optimization

---
+++ aoc-19/src/main.rs
@@ -0,0 +1,134 @@
+use std::collections::HashMap;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+enum Resource {
+    Ore,
+    Clay,
+    Obsidian,
+    Geode
+}
+
+type Vec4d = [i32;4];
+
+fn affordable(cost: &Vec<i32>, resources: &Vec4d) -> bool {
+    for i in 0..cost.len() {
+        if cost[i] > resources[i] { return false;}
+    }
+
+    true
+}
+
+fn resources_add(r1: &Vec4d, r2: &Vec4d) -> Vec<i32> {
+    let mut remainder = vec![];
+    for i in 0..r1.len() {
+        remainder.push(r1[i]+r2[i]);
+    }
+
+    remainder
+}
+
+fn resources_sub(cost: &Vec4d, resources: &Vec4d) -> Vec<i32> {
+    let mut remainder = vec![];
+    for i in 0..cost.len() {
+        remainder.push(resources[i]-cost[i]);
+    }
+
+    remainder
+}
+
+fn get_elements_for_robot_idx() -> &'static [Vec4d;4] {
+    &[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+}
+
+fn best_plan(answer_space: &mut HashMap<(Vec4d,Vec4d,i32),i32>, factory: &Vec<Vec<i32>>, resources: &Vec4d, robots: &Vec4d, turns_left: i32) -> i32 {
+    match answer_space.get(&(*resources,*robots,turns_left)) {
+        Some(cached_answer) => { return *cached_answer; }
+        _ => {
+            let mut computed_answer = -1;
+            if turns_left==0 { 
+                computed_answer = resources[3]; 
+            }
+            else {
+                let new_resources = resources_add( resources, &robots ); 
+                for (i, robot_choice) in factory.iter().enumerate() {
+                    if affordable(robot_choice, resources) {
+                        let remainder = resources_sub(robot_choice, &new_resources);
+                        let new_robots = resources_add(robots, &get_elements_for_robot_idx()[i]);
+                        let this_choice = best_plan( answer_space, &factory, &remainder, &new_robots, turns_left-1);
+                        computed_answer = std::cmp::max(computed_answer, this_choice);
+                    }
+                }
+                // or build nothing this round
+                let last_choice = best_plan( answer_space, &factory, &new_resources, &robots, turns_left-1);
+                computed_answer = std::cmp::max(computed_answer, last_choice);
+                if turns_left >= 19 {
+                    println!("Turns left {turns_left}");
+                }
+            }
+            answer_space.insert((resources.clone(),robots.clone(),turns_left), computed_answer);
+
+            return computed_answer;
+        }
+    };
+}
+
+#[test]
+fn test_best_plan() {
+    println!("Hello?");
+    let factory = vec![vec![4,0,0,0],vec![2,0,0,0],vec![3,14,0,0],vec![2,0,7,0]];
+    let mut answer_space = HashMap::<(Vec<i32>,Vec<i32>,i32),i32>::new();
+    let answer = best_plan(&mut answer_space, &factory, &vec![5,37,6,7], &vec![1,4,2,2], 1);
+    assert_eq!(9, answer);    
+    let answer = best_plan(&mut answer_space, &factory, &vec![4,33,4,5], &vec![1,4,2,2], 2);
+    assert_eq!(9, answer);        
+    let answer = best_plan(&mut answer_space, &factory, &vec![3,29,2,3], &vec![1,4,2,2], 3);
+    assert_eq!(9, answer); 
+    let answer = best_plan(&mut answer_space, &factory, &vec![4,25,7,2], &vec![1,4,2,1], 4);
+    assert_eq!(9, answer); 
+    let answer = best_plan(&mut answer_space, &factory, &vec![0,0,0,0], &vec![1,0,0,0], 24);
+}
+
+fn parse_input(input: &str) {
+    for 
+}
+
+fn get_sample_input() -> &'static str {
+"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian."
+}
+
+fn get_puzzle_input() -> &'static str {
+"Blueprint 1: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 16 clay. Each geode robot costs 2 ore and 9 obsidian.
+Blueprint 2: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 14 clay. Each geode robot costs 2 ore and 16 obsidian.
+Blueprint 3: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 12 clay. Each geode robot costs 3 ore and 8 obsidian.
+Blueprint 4: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 14 clay. Each geode robot costs 3 ore and 8 obsidian.
+Blueprint 5: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 15 clay. Each geode robot costs 3 ore and 7 obsidian.
+Blueprint 6: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 8 clay. Each geode robot costs 2 ore and 10 obsidian.
+Blueprint 7: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 3 ore and 16 clay. Each geode robot costs 3 ore and 14 obsidian.
+Blueprint 8: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 7 clay. Each geode robot costs 3 ore and 8 obsidian.
+Blueprint 9: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 17 clay. Each geode robot costs 3 ore and 11 obsidian.
+Blueprint 10: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 3 ore and 6 clay. Each geode robot costs 4 ore and 11 obsidian.
+Blueprint 11: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 9 clay. Each geode robot costs 3 ore and 15 obsidian.
+Blueprint 12: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 14 clay. Each geode robot costs 3 ore and 16 obsidian.
+Blueprint 13: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 8 clay. Each geode robot costs 4 ore and 14 obsidian.
+Blueprint 14: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 19 clay. Each geode robot costs 2 ore and 12 obsidian.
+Blueprint 15: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 9 clay. Each geode robot costs 3 ore and 9 obsidian.
+Blueprint 16: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 4 ore and 19 clay. Each geode robot costs 4 ore and 12 obsidian.
+Blueprint 17: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 17 clay. Each geode robot costs 4 ore and 16 obsidian.
+Blueprint 18: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 7 clay. Each geode robot costs 2 ore and 9 obsidian.
+Blueprint 19: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 18 clay. Each geode robot costs 4 ore and 12 obsidian.
+Blueprint 20: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 3 ore and 19 clay. Each geode robot costs 4 ore and 13 obsidian.
+Blueprint 21: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 19 clay. Each geode robot costs 2 ore and 12 obsidian.
+Blueprint 22: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 16 clay. Each geode robot costs 2 ore and 18 obsidian.
+Blueprint 23: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 13 clay. Each geode robot costs 2 ore and 9 obsidian.
+Blueprint 24: Each ore robot costs 2 ore. Each clay robot costs 2 ore. Each obsidian robot costs 2 ore and 10 clay. Each geode robot costs 2 ore and 11 obsidian.
+Blueprint 25: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 16 clay. Each geode robot costs 3 ore and 14 obsidian.
+Blueprint 26: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 18 clay. Each geode robot costs 2 ore and 19 obsidian.
+Blueprint 27: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 20 clay. Each geode robot costs 4 ore and 7 obsidian.
+Blueprint 28: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 4 ore and 18 clay. Each geode robot costs 4 ore and 11 obsidian.
+Blueprint 29: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 4 ore and 8 clay. Each geode robot costs 3 ore and 7 obsidian.
+Blueprint 30: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 4 ore and 20 clay. Each geode robot costs 4 ore and 8 obsidian."
+}
